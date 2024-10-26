9add159600aaafb13e6f7d6d8cdf9e16b1819822
mingmingrr <mingmingrr@gmail.com>
2024-10-26 17:06:07 -0400

    P.seq: add overloads

--- src/minipyparse.py
+++ src/minipyparse.py
@@ -7,6 +7,26 @@ import re
 S = TypeVar('S')
 T = TypeVar('T')
 U = TypeVar('U')
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
+T3 = TypeVar('T3')
+T4 = TypeVar('T4')
+T5 = TypeVar('T5')
+T6 = TypeVar('T6')
+T7 = TypeVar('T7')
+T8 = TypeVar('T8')
+T9 = TypeVar('T9')
+T10 = TypeVar('T10')
+T11 = TypeVar('T11')
+T12 = TypeVar('T12')
+T13 = TypeVar('T13')
+T14 = TypeVar('T14')
+T15 = TypeVar('T15')
+T16 = TypeVar('T16')
+T17 = TypeVar('T17')
+T18 = TypeVar('T18')
+T19 = TypeVar('T19')
+T20 = TypeVar('T20')
 PF = Callable[[Sequence[S],int],Iterable[Tuple[int,T]]]
 
 L = Optional[Tuple[T,'L[T]']]
@@ -43,10 +63,6 @@ class P(Generic[S,T]):
 		return P(lambda s, n: next(([r] for r in self.f(s, n)), []))
 	def alter(self, p:P[S,U]) -> P[S,Union[T,U]]:
 		return P(lambda s, n: itertools.chain(self.f(s, n), p.f(s, n)))
-	@staticmethod
-	def seq(*xs:P[S,T]) -> P[S,L[T]]:
-		return functools.reduce(lambda p, x:
-			x.bind(lambda a: p.fmap(lambda b: (a, b))), reversed(xs), P.pure(None))
 	def maybe(self, default:U=cast(Any,None)) -> P[S,Union[T,U]]:
 		return self.alter(P.pure(default))
 	def many(self) -> P[S,list[T]]:
@@ -68,7 +84,7 @@ class P(Generic[S,T]):
 		return next(P(lambda s, n: [(n + 1, s[n])] if n < len(s) and p(s[n]) else [])
 			for p in [f if callable(f) else lambda x: f == x])
 	@staticmethod
-	def chunk(s:Sequence[S]) -> P[S,L[S]]:
+	def chunk(s:Sequence[S]) -> P[S,tuple[S,...]]:
 		return P.seq(*map(lambda c: P.single(lambda x: x == c), s))
 	@staticmethod
 	def choice(*xs:P[S,T]) -> P[S,T]:
@@ -86,6 +102,303 @@ class P(Generic[S,T]):
 		return next(p for p in [cast('P[S,T]', P())] for q in [f(p)] for p.f in [q.f])
 	def parse(self, s, i=0, just=False):
 		return (x[-1] if just else x for x in self.f(s, i))
+
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1],
+	) -> P[S,Tuple[T1]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2],
+	) -> P[S,Tuple[T1,T2]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3],
+	) -> P[S,Tuple[T1,T2,T3]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+	) -> P[S,Tuple[T1,T2,T3,T4]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5], x6:P[S,T6],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+		x9:P[S,T9], x10:P[S,T10], x11:P[S,T11],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+		x9:P[S,T9], x10:P[S,T10], x11:P[S,T11], x12:P[S,T12],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+		x17:P[S,T17],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+		x17:P[S,T17],
+		x18:P[S,T18],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16], x17:P[S,T17], x18:P[S,T18], x19:P[S,T19],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19]]: ...
+	@overload
+	@staticmethod
+	def seq(
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16], x17:P[S,T17], x18:P[S,T18], x19:P[S,T19], x20:P[S,T20],
+	) -> P[S,Tuple[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19,T20]]: ...
+	@overload
+	@staticmethod
+	def seq(*xs:P[S,T]) -> P[S,Tuple[T,...]]: ...
+	@staticmethod
+	def seq(*xs): # type: ignore
+		return functools.reduce(lambda p, x: x.bind(lambda a:
+			p.fmap(lambda b: (a, b))), reversed(xs), P.pure(None)).fmap(flattenT)
+
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1],U],
+		x1:P[S,T1],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2],U],
+		x1:P[S,T1], x2:P[S,T2],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5], x6:P[S,T6],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+		x9:P[S,T9], x10:P[S,T10], x11:P[S,T11],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4],
+		x5:P[S,T5], x6:P[S,T6], x7:P[S,T7], x8:P[S,T8],
+		x9:P[S,T9], x10:P[S,T10], x11:P[S,T11], x12:P[S,T12],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+		x17:P[S,T17],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16],
+		x17:P[S,T17],
+		x18:P[S,T18],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16], x17:P[S,T17], x18:P[S,T18], x19:P[S,T19],
+	) -> P[S,U]: ...
+	@overload
+	@staticmethod
+	def apply(
+		f:Callable[[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19,T20],U],
+		x1:P[S,T1], x2:P[S,T2], x3:P[S,T3], x4:P[S,T4], x5:P[S,T5],
+		x6:P[S,T6], x7:P[S,T7], x8:P[S,T8], x9:P[S,T9], x10:P[S,T10],
+		x11:P[S,T11], x12:P[S,T12], x13:P[S,T13], x14:P[S,T14], x15:P[S,T15],
+		x16:P[S,T16], x17:P[S,T17], x18:P[S,T18], x19:P[S,T19], x20:P[S,T20],
+	) -> P[S,U]: ...
+	@staticmethod
+	def apply(f, *xs): # type: ignore
+		return P.seq(*xs).fmap(lambda ns: f(*ns))
+
 	def __pos__    (self): return self.some()
 	def __invert__ (self): return self.many()
 	def __pow__    (self, n): return self.replace(n)
